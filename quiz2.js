878e07bec01b93ea3a71014bfa1a4e95ee35194a
ziLun19 <b0829049@cgu.edu.tw>
2021-04-06 21:07:53 +0800

    Create quiz2.js

---
+++ quiz2.js
@@ -0,0 +1,16 @@
+var tags = "李知恩";
+
+var dataUrl = "https://api.flickr.com/services/feeds/photos\_public.gne?tags=" + tags
++ "&tagmode=any&format=json&per_page=400&jsoncallback=?"; 
+var data = $.getJSON(dataUrl);
+data.done(function (msg) {
+    $.each(msg.items, function (i, item) {
+        $("#contain").html();
+        $("#contain").append($("<img/>").attr("src", item.media.m));
+    });
+});
+
+data.fail(function (msg) {
+    console.log(msg);
+    $("#contain").html("fail getting data");
+});
